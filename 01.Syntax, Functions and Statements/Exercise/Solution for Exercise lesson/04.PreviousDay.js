f9cc02bca203287b064640b3bbfbfb22af711cff
Valentin Taslakov <valentintaslakov@gmail.com>
2022-09-17 11:09:41 +0300

    rename folder

--- 01.Syntax, Functions and Statements/Exercise/Solution for Exercise lesson/04.PreviousDay.js
+++
@@ -1,9 +0,0 @@
-function previousDay(year, month, day){
-    let curentDate = new Date(year, month-1 , day);
-
-    let previousDate = new Date(curentDate -1);
-
-    console.log(`${previousDate.getFullYear()}-${previousDate.getMonth()+1}-${previousDate.getDate()}`);
-}
-
-previousDay(2016, 10, 1);
