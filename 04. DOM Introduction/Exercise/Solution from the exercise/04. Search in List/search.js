88c4fac63568b5f889d6ffaa2b82a9d20f058d9a
Valentin Taslakov <valentintaslakov@gmail.com>
2022-10-06 20:11:38 +0300

    Solution for tasks 4 and 5

--- 04. DOM Introduction/Exercise/Solution from the exercise/04. Search in List/search.js
+++ 04. DOM Introduction/Exercise/Solution from the exercise/04. Search in List/search.js
@@ -1,3 +1,57 @@
 function search() {
-   // TODO
+   let towns = document.querySelectorAll("ul li");
+   // тук горното взима всички ul и от тях всички li, 
+   // в задачата имаме само едно ul но ако имаме повече 
+   // трябва да използваме ид - то като ще изглежда така ("#towns li")
+   let searchText = document.getElementById("searchText").value;
+   let matches = 0;
+
+   for (const li of towns) {
+
+      let town = (li.textContent);
+      let reg = new RegExp(searchText, "i");
+      reg.ignoreCase;
+
+      if ((searchText)!=="" && reg.test(town)) {
+         li.style.fontWeight = "bold";
+         li.style.textDecoration = "underline";
+         matches += 1;
+      }else{
+         li.style.fontWeight = "";
+         li.style.textDecoration = "";
+         //с този елсе премахвам предишните резултати
+         //но все още когато е празно полето за търсене ми 
+         //маркира всичко
+      }
+      document.getElementById("result").textContent = `${matches} matches found`;
+      //вместо textContent мога да използвам и .innerText
+   }
+   ////console.log(searchText);
 }
+//function search() {
+//   let towns = document.querySelectorAll("#towns li");
+//
+//   let searchText = document.getElementById("searchText").value;
+//   let matches = 0;
+//
+//   for (const li of towns) {
+//
+//      let town = (li.textContent);
+//      let reg = new RegExp(searchText, "i");
+//      reg.ignoreCase;
+//
+//      if (town.includes(searchText)) {
+//         li.style.fontWeight = "bold";
+//         li.style.textDecoration = "underline";
+//         matches += 1;
+//      } else {
+//         li.style.fontWeight = "";
+//         li.style.textDecoration = "";
+//
+//      }
+//      document.getElementById("result").textContent = `${matches} matches found`;
+//// и решението на лектора не покрива всичко даже грешките са повече
+//// ако напишем една буква и тя се съдържа посредата на няколко града 
+//// ще ги маркира всички
+//   }
+//}
--- 04. DOM Introduction/Exercise/Solution from the exercise/05. Table - Search Engine/solution.js
+++ 04. DOM Introduction/Exercise/Solution from the exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,57 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+
+   document.querySelector('#searchBtn')
+      .addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      let rows = document.querySelectorAll("table tr");
+      // взимам всички редове от таблицата 
+      // заедно със заглавните клетки
+      let searchField = document.getElementById("searchField").value;
 
+      for (let i = 1; i < rows.length; i++) {
+      // пускам фор като започвам от 1 за да избегна 
+      //заглавните клетки
+
+         let cols = rows[i].children;
+      //взимам колоните от реда
+
+         rows[i].classList.remove("select")
+      //classList е лист който държи атрибутите на елемент или
+      // лист от className на елеелемент в HTML, ако HTML ел. няма 
+      //атрибут class="container" списъка е празен
+      
+         for (let j = 0; j < cols.length; j++) {
+            
+            if (cols[j].textContent.includes(searchField)) {
+               rows[i].className = "select";
+            //rows[i].classList.add("select"); горния ред става
+            // и по този начин или към className атрибута 
+            //добавяме още една стойност която индикира
+            //на CSS да добави друга функционалност на 
+            //HTML елемента, а именно друго оцветяване или 
+            //както сме настроили CSS
+            }
+         }
+      }
+      searchField.value = '';
    }
 }
+
+//function solve() {
+//   document.querySelector('#searchBtn').addEventListener('click', onClick);
+// 
+//   function onClick() {
+//     let searchField = document.getElementById('searchField');
+//     let rows = document.querySelectorAll('tbody tr');
+// 
+//     for (const row of rows) {
+//       row.classList.remove('select');
+// 
+//       if (row.innerHTML.includes(searchField.value)) {
+//         row.className = 'select';
+//       }
+//     }
+//     searchField.value = '';
+//   }
+// }
